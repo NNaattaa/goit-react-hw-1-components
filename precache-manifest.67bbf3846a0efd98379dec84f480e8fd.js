f680afd4ece5b2a95274222ca0327ae72260d104
NNaattaa <50961364+NNaattaa@users.noreply.github.com>
2020-06-25 06:49:48 +0300

    Updates

---
+++ precache-manifest.67bbf3846a0efd98379dec84f480e8fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77dcb7190c72e1836d0373f36b1c37d5",
+    "url": "/goit-react-hw-1-components/index.html"
+  },
+  {
+    "revision": "ccef7e744c3e2a9cb2b3",
+    "url": "/goit-react-hw-1-components/static/css/main.5bedd330.chunk.css"
+  },
+  {
+    "revision": "99919af9b0d94aaa0bb8",
+    "url": "/goit-react-hw-1-components/static/js/2.befd264b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-1-components/static/js/2.befd264b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccef7e744c3e2a9cb2b3",
+    "url": "/goit-react-hw-1-components/static/js/main.74e609a7.chunk.js"
+  },
+  {
+    "revision": "fc394c4bb03a82045024",
+    "url": "/goit-react-hw-1-components/static/js/runtime-main.b29ed98a.js"
+  }
+]);
